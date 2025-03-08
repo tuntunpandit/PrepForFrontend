9272ece29a0808476032cf178ca7960bb61ae617
tuntunpandit <tuntunkumarjamui@gmail.com>
2025-03-08 11:45:40 +0530

    project setup

---
+++ src/app/components/User/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss'],
+})
+export class UserComponent {
+  users: any[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
+}
