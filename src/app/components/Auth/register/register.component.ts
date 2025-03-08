4df2e3de5e58396f180f7e5a8e2d28f178ed62d8
tuntunpandit <tuntunkumarjamui@gmail.com>
2025-03-08 16:06:23 +0530

    login and register page code updated

--- src/app/components/Auth/register/register.component.ts
+++ src/app/components/Auth/register/register.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  registerForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+  onSubmit(): void {
+    console.log(this.registerForm.value);
+  }
 }
